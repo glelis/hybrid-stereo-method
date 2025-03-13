c1b5a489b4514d0e7b88f80062be6891e03f6ff4
glelis <g.lelis.silva@gmail.com>
2025-03-13 15:45:21 +0100

    update: change image source paths and adjust configuration files for new dataset

--- src/common/copy_images_ps.sh
+++ src/common/copy_images_ps.sh
@@ -4,14 +4,15 @@
 #run: ./copy_images.sh 
 
 # Source directory containing images
-DIR="/home/lelis/Documents/Projetos/hybrid-stereo-method/data/raw/hybrid_stereo/2025-02-11-stQ-melon24-amb0.00-glo0.50/"
+DIR="/home/lelis/Documents/Projetos/hybrid-stereo-method/data/raw/hybrid_stereo/2025-03-08-stQ-melon24-amb0.00-glo0.50/"
+'stQ-melon14-amb0.00-glo0.50/0512x0384-hs01-kr10/L000/sharp'
 
 
 mkdir -p $DIR/images_ps;
 src/common/copy_images.sh
 for i in 00 01 02 03 04 05 06 07 08 09 10 11; do 
 	
-	cp -v "$DIR/stQ-melon24/0512x0384-hs01-kr10/L0$i-amb0.00-glo0.50/sharp/sVal.png" "$DIR/images_ps/image$i.png"; 
+	cp -v "$DIR/stQ-melon14-amb0.00-glo0.50/0512x0384-hs01-kr10/L0$i/sharp/sVal.png" "$DIR/images_ps/image$i.png"; 
 done
 
 echo "Copied images to images_ps directory!"
